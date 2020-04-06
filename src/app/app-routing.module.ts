48ddb3f483ede6968ad22ef9bd1e1934e4c3378e
Dis <d.ndocum@gmail.com>
2020-04-06 19:26:15 +0300

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ErroComponent} from './erro/erro.component';
 import { from } from 'rxjs';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { MyAccountComponent } from './pages/my-account/my-account.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CompareComponent } from './pages/compare/compare.component';
+import { OrderTrackingComponent } from './pages/order-tracking/order-tracking.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
 const routes: Routes = [
-  {path: 'too', component: ErroComponent}
+  {path: '', component: HomeComponent},
+  {path: 'Shop', component: ShopComponent},
+  {path: 'MyAccount', component: MyAccountComponent},
+  {path: 'Checkout', component: CheckoutComponent},
+  {path: 'Cart', component: CartComponent},
+  {path: 'Compare', component: CompareComponent},
+  {path: 'OrderTracking', component: OrderTrackingComponent},
+  {path: 'Wishlist', component: WishlistComponent},
+  {path: '404', component: Page404Component},
+  {path: 'Blog', component: BlogComponent},
+  {path: 'ContactUs', component: ContactUsComponent},
+  {path: 'LoginRegister', component: LoginRegisterComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ErroComponent } from './erro/erro.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { BlogComponent } from './blog/blog.component';
 import { ShopComponent } from './shop/shop.component';
-import { MyAccountComponent } from './my-account/my-account.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { CartComponent } from './cart/cart.component';
-import { CompareComponent } from './compare/compare.component';
-import { OrderTrackingComponent } from './order-tracking/order-tracking.component';
-import { WishlistComponent } from './wishlist/wishlist.component';
+import { MyAccountComponent } from './pages/my-account/my-account.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CompareComponent } from './pages/compare/compare.component';
+import { OrderTrackingComponent } from './pages/order-tracking/order-tracking.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
 import { LoginRegisterComponent } from './login-register/login-register.component';
 import { HomeComponent } from './home/home.component';
-import { Page404Component } from './page404/page404.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErroComponent,
     ContactUsComponent,
     BlogComponent,
     ShopComponent,
